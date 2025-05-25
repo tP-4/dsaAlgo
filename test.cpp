279e2f743c471d10c5db5f9cc6d60e89e19f3ae8
T Q <testingpurp4@gmail.com>
2025-05-25 08:58:23 +0530

    initial commit for dsaAlgo practice

---
+++ test.cpp
@@ -0,0 +1,18 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int x;
+//     cin >> x;
+//     cout << "hi" << x;
+// }
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    cout << "val of x:" << x;
+    return 0;
+}
